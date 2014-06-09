ba8a0f07fb7a100181d5fef77f3ef392181bf393
vagrant <vagrant@student-dev-box>
2014-06-09 10:13:59 -0500

    ÃÃadded third test and functionality

--- spec/exercises_spec.rb
+++ spec/exercises_spec.rb
@@ -3,15 +3,30 @@ require 'spec-helper'
 
 describe Exercises do 
 
-	it ':Triples a given string ' do
-		str = Exercises.ex0("hello")
+	describe ".ex0" do
+		it ':Triples a given string ' do
+			str = Exercises.ex0("hello")
 
-		expect(str).to eq("hellohellohello")
+			expect(str).to eq("hellohellohello")
+		end
+
+		it 'Returns "nope" if str is wishes' do
+			expect(Exercises.ex0("wishes")).to eq("nope")
+		end
 	end
 
-	it 'Returns "nope" if str is wishes' do
-		expect(Exercises.ex0("wishes")).to eq("nope")
-	end
+	describe '.ex1' do
+    it 'Returns the number of elements in the array' do
+      
+      arr = [0,1,2]
+
+      expect(Exercises.ex1(arr)).to eq(3)
+    end
+  end
+
+
+
+
 end
 
 
